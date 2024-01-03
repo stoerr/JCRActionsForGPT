2ddd12d6749422a57551c6aa6d60016478c33b31
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2024-01-03 18:14:35 +0000

    ChatGPT: Use request.getRequestPathInfo instead and dispatch to separate methods according to the extension. Use Java 8 syntax. Run the build and fix any problems.

--- src/main/java/net/stoerr/chatgpt/jcractions/GPTJCRActionsServlet.java
+++ src/main/java/net/stoerr/chatgpt/jcractions/GPTJCRActionsServlet.java
@@ -1,9 +1,7 @@
 package net.stoerr.chatgpt.jcractions;
 
 import java.io.IOException;
-
 import javax.servlet.Servlet;
-
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.HttpConstants;
@@ -16,15 +14,44 @@ import org.osgi.service.component.annotations.Component;
         property = {
                 Constants.SERVICE_DESCRIPTION + "=net.stoerr.chatgpt JCR Actions Servlet",
                 ServletResolverConstants.SLING_SERVLET_PATHS + "=/bin/gpt/jcractions",
-                ServletResolverConstants.SLING_SERVLET_METHODS + "=" + HttpConstants.METHOD_GET,
-                ServletResolverConstants.SLING_SERVLET_METHODS + "=" + HttpConstants.METHOD_POST
+                ServletResolverConstants.SLING_SERVLET_METHODS + "=" + HttpConstants.METHOD_GET
         })
 public class GPTJCRActionsServlet extends SlingAllMethodsServlet {
 
     @Override
     protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
-        response.setContentType("text/plain");
-        response.getWriter().write("GPT JCR Actions Servlet is operational.");
+        String extension = request.getRequestPathInfo().getExtension();
+
+        switch (extension) {
+            case "yaml":
+                serveOpenAPISpecification(request, response);
+                break;
+            case "json":
+                serveJSONRepresentation(request, response);
+                break;
+            case "data":
+                serveBinaryData(request, response);
+                break;
+            default:
+                response.setContentType("text/plain");
+                response.getWriter().write("Invalid request extension.");
+                break;
+        }
+    }
+
+    private void serveOpenAPISpecification(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
+        response.setContentType("application/yaml");
+        response.getWriter().write("OpenAPI spec content here...");
+    }
+
+    private void serveJSONRepresentation(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        response.getWriter().write("JSON content for " + request.getRequestPathInfo().getResourcePath() + " here...");
+    }
+
+    private void serveBinaryData(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
+        response.setContentType("binary/data-type"); // set appropriate mime type
+        response.getWriter().write("Binary data for " + request.getRequestPathInfo().getResourcePath() + " here...");
     }
 
 }
