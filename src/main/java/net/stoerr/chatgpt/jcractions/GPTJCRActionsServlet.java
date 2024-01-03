e74b3854dc070216efff89853536ab6f25f1970b
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2024-01-03 20:01:30 +0000

    ChatGPT: Implement the servlet functions for yaml. Generate an OpenAPI specification and put it as constant into the servlet. Format the string constant so that it is readable in the source code - split it into lines. Create the yaml for all operations as described in the specification. Use the replaceInFile action to make the changes.

--- src/main/java/net/stoerr/chatgpt/jcractions/GPTJCRActionsServlet.java
+++ src/main/java/net/stoerr/chatgpt/jcractions/GPTJCRActionsServlet.java
@@ -1,7 +1,9 @@
 package net.stoerr.chatgpt.jcractions;
 
 import java.io.IOException;
+
 import javax.servlet.Servlet;
+
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.HttpConstants;
@@ -10,9 +12,9 @@ import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Modified;
-import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.metatype.annotations.Designate;
 
 @Component(service = Servlet.class,
@@ -61,7 +63,45 @@ public class GPTJCRActionsServlet extends SlingAllMethodsServlet {
 
     private void serveOpenAPISpecification(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
         response.setContentType("application/yaml");
-        response.getWriter().write("OpenAPI spec content here...");
+        response.getWriter().write("" +
+                "openapi: 3.0.0\n" +
+                "info:\n" +
+                "  title: GPT JCR Actions API\n" +
+                "  description: API to interact with JCR content repository via GPT.\n" +
+                "  version: 1.0.0\n" +
+                "servers:\n" +
+                "  - url: /bin/gpt/jcractions\n" +
+                "paths:\n" +
+                "  /jcractions.yaml:\n" +
+                "    get:\n" +
+                "      summary: Returns the OpenAPI specification\n" +
+                "      responses:\n" +
+                "        '200':\n" +
+                "          description: OK\n" +
+                "  /jcractions.json/{path}:\n" +
+                "    get:\n" +
+                "      summary: Returns JSON representation of the JCR node\n" +
+                "      parameters:\n" +
+                "        - in: path\n" +
+                "          name: path\n" +
+                "          required: true\n" +
+                "          schema:\n" +
+                "            type: string\n" +
+                "      responses:\n" +
+                "        '200':\n" +
+                "          description: JSON content of the JCR node\n" +
+                "  /jcractions.data/{path}:\n" +
+                "    get:\n" +
+                "      summary: Returns the binary data of the JCR node\n" +
+                "      parameters:\n" +
+                "        - in: path\n" +
+                "          name: path\n" +
+                "          required: true\n" +
+                "          schema:\n" +
+                "            type: string\n" +
+                "      responses:\n" +
+                "        '200':\n" +
+                "          description: Binary data of the JCR node\n");
     }
 
     private void serveJSONRepresentation(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
