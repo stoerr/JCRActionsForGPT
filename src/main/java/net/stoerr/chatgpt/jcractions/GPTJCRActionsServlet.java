8eb320e6ade2c3398177fa710fba296204f4aebe
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2024-01-04 17:44:30 +0000

    add children nodes to json and add depth parameter to json

--- src/main/java/net/stoerr/chatgpt/jcractions/GPTJCRActionsServlet.java
+++ src/main/java/net/stoerr/chatgpt/jcractions/GPTJCRActionsServlet.java
@@ -2,11 +2,16 @@ package net.stoerr.chatgpt.jcractions;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.stream.Stream;
 
 import javax.servlet.Servlet;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.request.RequestPathInfo;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.servlets.HttpConstants;
 import org.apache.sling.api.servlets.ServletResolverConstants;
@@ -32,7 +37,23 @@ import com.google.gson.JsonObject;
 @Designate(ocd = GPTJCRActionsConfig.class)
 public class GPTJCRActionsServlet extends SlingAllMethodsServlet {
 
+    /**
+     * Those are metadata and not relevant for the sresource content.
+     */
+    private static final Collection<String> ignoredMetadataAttributes = new HashSet<>(Arrays.asList("jcr:uuid", "jcr:lastModified",
+            "jcr:lastModifiedBy", "jcr:created", "jcr:createdBy", "jcr:isCheckedOut", "jcr:baseVersion",
+            "jcr:versionHistory", "jcr:predecessors", "jcr:mergeFailed", "jcr:mergeFailed", "jcr:configuration",
+            "jcr:activity", "jcr:etag", "rep:hold", "rep:retentionPolicy", "rep:versions",
+            "jcr:data", // handled with /jcractions.data
+            // AEM specific:
+            "cq:lastModified", "cq:lastModifiedBy", "cq:lastReplicated", "cq:lastReplicatedBy",
+            "cq:lastReplicationAction", "cq:lastReplicationStatus",
+            "cq:lastRolledout", "cq:lastRolledoutBy", "cq:lastRolledoutAction", "cq:lastRolledoutStatus"
+    ));
+
     private volatile transient GPTJCRActionsConfig config;
+
+    private final transient Gson gson = new Gson();
 
     @Activate
     @Modified
@@ -70,7 +91,7 @@ public class GPTJCRActionsServlet extends SlingAllMethodsServlet {
      * Returns a full OpenAPI spec of this servlet; we omit /jcractions.yaml since that is not an operation for the GPT.
      */
     private void serveOpenAPISpecification(SlingHttpServletResponse response) throws IOException {
-        response.setContentType("application/yaml");
+        response.setContentType("text/yaml");
         response.getWriter().write("" +
                 "openapi: 3.0.0\n" +
                 "info:\n" +
@@ -80,7 +101,7 @@ public class GPTJCRActionsServlet extends SlingAllMethodsServlet {
                 "servers:\n" +
                 "  - url: /bin/gpt/jcractions\n" +
                 "paths:\n" +
-                "  /jcractions.json/{path}:\n" +
+                "  /jcractions.{depth}.json/{path}:\n" +
                 "    get:\n" +
                 "      summary: Returns JSON representation of the JCR node\n" +
                 "      parameters:\n" +
@@ -89,6 +110,12 @@ public class GPTJCRActionsServlet extends SlingAllMethodsServlet {
                 "          required: true\n" +
                 "          schema:\n" +
                 "            type: string\n" +
+                "        - in: path\n" +
+                "          name: depth\n" +
+                "          required: true\n" +
+                "          schema:\n" +
+                "            type: integer\n" +
+                "            format: int32\n" +
                 "      responses:\n" +
                 "        '200':\n" +
                 "          description: JSON content of the JCR node\n" +
@@ -107,11 +134,49 @@ public class GPTJCRActionsServlet extends SlingAllMethodsServlet {
     }
 
     private void serveJSONRepresentation(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
+        RequestPathInfo requestPathInfo = request.getRequestPathInfo();
+        Resource resource = requestPathInfo.getSuffixResource();
+        if (resource == null) {
+            response.setStatus(404);
+            response.getWriter().write("No resource found for " + request.getRequestPathInfo().getResourcePath());
+            return;
+        }
+        // find and parse numeric selector as depth
+        int depth = Stream.of(requestPathInfo.getSelectors())
+                .filter(s -> s.matches("\\d+"))
+                .map(Integer::parseInt)
+                .findFirst()
+                .orElse(0);
         response.setContentType("application/json");
-        Resource resource = request.getRequestPathInfo().getSuffixResource();
+        JsonObject json = toJsonObject(resource, depth);
+        response.getWriter().write(gson.toJson(json));
+    }
+
+    private JsonObject toJsonObject(Resource resource, int depth) {
         JsonObject json = new JsonObject();
-        resource.getValueMap().forEach((key, value) -> json.addProperty(key, String.valueOf(value)));
-        response.getWriter().write(new Gson().toJson(json));
+        resource.getValueMap().entrySet().stream()
+                .filter(entry -> !ignoredMetadataAttributes.contains(entry.getKey()))
+                .forEach(entry -> addProperty(json, entry.getKey(), entry.getValue()));
+        if (depth > 0) {
+            resource.getChildren().forEach(child -> json.add(child.getName(), toJsonObject(child, depth - 1)));
+        }
+        return json;
+    }
+
+    private void addProperty(JsonObject json, String key, Object value) {
+        // if it's an array, we add all elements as json array
+        if (value instanceof Object[]) {
+            Object[] array = (Object[]) value;
+            json.add(key, gson.toJsonTree(array));
+        } else if (value instanceof Number) {
+            json.addProperty(key, (Number) value);
+        } else if (value instanceof Boolean) {
+            json.addProperty(key, (Boolean) value);
+        } else if (value instanceof String) {
+            json.addProperty(key, (String) value);
+        } else {
+            json.addProperty(key, String.valueOf(value));
+        }
     }
 
     private void serveBinaryData(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
